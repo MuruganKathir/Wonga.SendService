7c179f14037d39ac5d64158a901648d75bda4391
Kathir <Kathir@websightseo.co.za>
2022-11-02 00:54:34 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using RabbitMQ.Client;
+using System;
+using Wonga.Common;
+using Wonga.SendMessage.Adapters.SendMessage;
+using Wonga.SendMessage.Contracts;
+using Wonga.SendMessage.Contracts.SendMessage;
+using Wonga.SendMessage.Orchestrations;
+
+namespace SendService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            var inputString = Console.ReadLine();
+
+            var sendMessageOrchestration = WongaFactory.CreateInstance<ISendMessageOrchestrations>();
+            var profile = sendMessageOrchestration.SendMessage(inputString);
+
+            //var factory = new ConnectionFactory
+            //{
+            //    Uri = new Uri("amqp://guest:guest@localhost:5672")
+            //};
+            //var connection = factory.CreateConnection();
+            //var channel = connection.CreateModel();
+            //DirectExchangePublisher.Publish(channel);
+
+            Console.Read();
+        }
+    }
+}
