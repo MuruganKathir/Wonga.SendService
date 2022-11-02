adab6f1b10440163ccf62b95d36757ddb218d66e
Kathir <Kathir@websightseo.co.za>
2022-11-02 06:45:12 +0200

    Wonga.Common project has been created to share  across applications. QueueOperationOutcome  defines outcome of direct exchange method invocation.

---
+++ Wonga.Common/Errors/Errors.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wonga.Common
+{
+    public static class Errors
+    {
+        /// <summary>
+        /// Gets the application not found.
+        /// </summary>
+        /// <value>
+        /// The application not found.
+        /// </value>
+        public static Error InvalidMessage
+        {
+            get
+            {
+                return new Error
+                {
+                    Code = "InvalidMessage",
+                    Description = "Invalid message on Send Method."
+                };
+            }
+        }
+        /// <summary>
+        /// ConnectionFailed
+        /// </summary>
+        public static Error ConnectionFailed
+        {
+            get
+            {
+                return new Error
+                {
+                    Code = "ConnectionFailed",
+                    Description = "Failed to Connect RabbitMQ Url."
+                }; 
+            }
+        }
+        /// <summary>
+        /// ChannelFailed
+        /// </summary>
+        public static Error ChannelFailed
+        {
+            get
+            {
+                return new Error
+                {
+                    Code = "ChannelFailed",
+                    Description = "Failed to Connect Channel Url."
+                };
+            }
+        }
+
+        /// <summary>
+        /// Gets the unexpected internal error error.
+        /// </summary>
+        /// <value>
+        /// The unexpected internal error error.
+        /// </value>
+        public static Error UnexpectedInternalError
+        {
+            get
+            {
+                return new Error { Code = "UnexpectedInternalError", Description = "Something unexpected went wrong." };
+            }
+        }
+    }
+}
---
+++ Wonga.Common/WongaFactory.cs
@@ -0,0 +1,20 @@
+﻿
+using Microsoft.Practices.Unity;
+using Microsoft.Practices.Unity.Configuration;
+
+namespace Wonga.Common
+{
+    /// <summary>
+    /// Wonga Factory Class which is incorporated factory pattern using Unity Container based on IOC(Inversion of Control principle)
+    /// </summary>
+    public static class WongaFactory
+    {
+        public static T CreateInstance<T>()
+        {
+            var container = new UnityContainer();
+            container.LoadConfiguration();
+            var resolvedObject = container.Resolve<T>();
+            return resolvedObject;
+        }
+    }
+}
