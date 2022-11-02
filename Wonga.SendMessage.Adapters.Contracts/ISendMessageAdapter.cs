40b805ebddaaa409f30eeb25cb1ca0b1aecef8ca
Kathir <Kathir@websightseo.co.za>
2022-11-02 06:48:30 +0200

    Message has been sent from ServiceA efficiently using adapter design pattern

---
+++ Wonga.SendMessage.Adapters.Contracts/ISendMessageAdapter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Wonga.Common;
+
+namespace Wonga.SendMessage.Adapters.Contracts
+{
+    public interface ISendMessageAdapter
+    {
+        /// <summary>
+        /// Send Message method from Adapter Contract
+        /// </summary>
+        /// <param name="fromProducer"></param>
+        /// <returns>Queue Operation outcome return either call successfull or not with data.</returns>
+        QueueOperationOutcome SendMessage(string fromProducer);
+    }
+}
---
+++ Wonga.SendMessage.Adapters/SendMessage/SendMessageAdapter.cs
@@ -0,0 +1,33 @@
+﻿using RabbitMQ.Client;
+using System;
+using Wonga.Common;
+using Wonga.SendMessage.Adapters.Contracts;
+using Wonga.SendMessage.Contracts.Errors;
+using Errors = Wonga.Common.Errors;
+
+namespace Wonga.SendMessage.Adapters.SendMessage
+{
+    public class SendMessageAdapter : ISendMessageAdapter
+    {
+        
+        public QueueOperationOutcome SendMessage(string fromProducer)
+        {
+            var factory = new ConnectionFactory
+            {
+                Uri = new Uri("amqp://guest:guest@localhost:5672")
+            };
+            var connection = factory.CreateConnection() ;
+            if (connection == null)
+            {
+                return QueueOperationOutcome.CreateError(Errors.ConnectionFailed);
+            }
+            var channel = connection.CreateModel();
+            if (channel == null)
+            {
+                return QueueOperationOutcome.CreateError(Errors.ChannelFailed);
+            }
+            DirectExchangePublisher.Publish(channel);
+            return QueueOperationOutcome.Success;           
+        }
+    }
+}
